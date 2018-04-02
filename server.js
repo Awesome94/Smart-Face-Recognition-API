a888ee19683c23178036720d311c8cfacb2c3f3f
David Kironde <david.kironde@andela.com>
2018-04-03 02:07:34 +0300

    add end points for the various requests

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
+
+app.use(bodyParser.json());
 
 const database = {
   users : [
@@ -23,7 +27,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.send(database.users);
 })
 
 app.post('/sigin', (req, res) =>{
@@ -33,6 +37,48 @@ app.post('/sigin', (req, res) =>{
   }else{
     res.status(400).json('error logging in')
   }
+})
+
+app.post('/register', (req,res)=>{
+  const{name, email, password} = req.body;
+  database.users.push({
+    id: 12345,
+    name: name,
+    email: email,
+    entries: 0,
+    password: password,
+    joined: new Date()
+  });
+  res.status(201).json(database.users[database.users.length-1])
+});
+
+app.get('/profile/:id', (req, res)=>{
+  const {id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id){
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found){
+    res.status(400).json('not found')
+  }
+})
+
+app.put('/image', (req, res) => {
+const { id } = req.body;
+let found = false;
+database.users.forEach(user => {
+  if(user.id === id){
+    found = true;
+    user.entries++
+    return res.json(user.entries);
+  }
+  if(!found){
+    res.status(404).json("User not found")
+  }
+})
 })
 
 app.listen(process.env.port  || 3000, function () {
