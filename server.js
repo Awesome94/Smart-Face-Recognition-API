c7676e8697980133c84bc0d73819b325ca7c3022
David Kironde <david.kironde@andela.com>
2018-04-04 13:48:56 +0300

    configure signin route/endpoint with the database

--- server.js
+++ server.js
@@ -44,13 +44,22 @@ app.get('/', (req, res) => {
   res.send(database.users);
 })
 
-app.post('/sigin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-    res.json('success...')
-  } else {
-    res.status(400).json('error logging in')
-  }
+app.post('/signin', (req, res) => {
+ db.select('email', 'hash'). from ('login')
+ .where('email', '=', req.body.email)
+ .then(data => {
+   const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+   if (isValid) {
+    return db.select('*').from('users')
+     .where('email', '=', req.body.email)
+     .then(user => {
+       res.json(user[0])
+     })
+     .catch(err => res.status(400).json('unable to get user'))
+   }
+   res.status(400).json('wrong credentials')
+ })
+ .catch(err => res.status(400).json('wrong creds'))
 })
 
 app.post('/register', (req, res) => {
