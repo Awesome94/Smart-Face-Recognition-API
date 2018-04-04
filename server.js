f63fb257e464c47a005400c9707f39ad715bab4b
David Kironde <david.kironde@andela.com>
2018-04-04 13:24:44 +0300

    hash passwords and add transaction(trx) for user and login table

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
-const db  = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 const database = {
-  users : [
+  users: [
     {
       id: '123',
       name: 'john',
@@ -44,56 +44,70 @@ app.get('/', (req, res) => {
   res.send(database.users);
 })
 
-app.post('/sigin', (req, res) =>{
-  if(req.body.email === database.users[0].email &&
-  req.body.password === database.users[0].password){
+app.post('/sigin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password) {
     res.json('success...')
-  }else{
+  } else {
     res.status(400).json('error logging in')
   }
 })
 
-app.post('/register', (req,res)=>{
-  const{name, email, password} = req.body;
-  db('users')
-  .returning('*')
-  .insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  }).then(user => {
-    res.json(user[0]);
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash,
+      email
+    })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          }).then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
   })
-  .catch(err => res.status(400).json('unable to register'))
+
+    .catch(err => res.status(400).json('unable to register'))
 });
 
-app.get('/profile/:id', (req, res)=>{
-  const {id } = req.params;
-  db.select('*').from('users').where({id})
-  .then(user => {
-    if (user.length){
-      res.json(user)
-    } else {
-      res.status(404).json(`user ${id} not found`)
-    }
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user)
+      } else {
+        res.status(404).json(`user ${id} not found`)
+      }
     })
-      .catch(err => res.status(400).json(`error getting user  ${id}`))
+    .catch(err => res.status(400).json(`error getting user  ${id}`))
 });
 
 app.put('/image', (req, res) => {
-const { id } = req.body;
-let found = false;
-db('users')
-.where('id', '=', id)
-.increment('entries', 1)
-.returning('entries')
-.then(entries => {
-  res.json(entries[0]);
-})
-.catch( err => res.status(400).json('Failed to get entries'))
+  const { id } = req.body;
+  let found = false;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Failed to get entries'))
 })
 
-app.listen(process.env.port  || 3000, function () {
+app.listen(process.env.port || 3000, function () {
   console.log("BASE URL : http://localhost:", this.address().port, app.settings.env);
   // console.log('sever statered on port', port)
 })
